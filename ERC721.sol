81cdd1e4b28361303977103b4cbd98a97c8ab45b
Muhammad-AhsanTariq <106869223+Muhammad-AhsanTariq@users.noreply.github.com>
2022-08-14 23:29:31 +0500

    Update ERC721.sol

--- ERC721.sol
+++ ERC721.sol
@@ -4,16 +4,10 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract MyNFT is ERC721URIStorage, Ownable {
-
-   
 
     constructor() ERC721("iecToken", "IT") {}
      
-    
-
-    
      uint private tokenId;
     function mintItem(address minter, string memory tokenURI, uint _tokenId)
          public onlyOwner returns (uint256) {
